40c988a4b3dcf33bcfc61a3e9c395f3b52d7e43f
Maciej Wilk <maciej.wilk@ringieraxelspringer.pl>
2023-04-20 09:15:29 +0200

    update configs

--- macos/HOME/enablesudotid.sh
+++ macos/HOME/enablesudotid.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 if ! grep 'pam_tid.so' /etc/pam.d/sudo --silent; then
+	echo "Enabling TouchID for sudo access"
 	awk 'NR==2 {print "auth       sufficient     pam_tid.so"} 1' /etc/pam.d/sudo | sudo dd of=/etc/pam.d/sudo
 fi
